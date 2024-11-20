a5215895e62e19aa85d2402c02fc468424726158
Ship-Vano <vanotheargonianin@gmail.com>
2024-11-20 19:09:24 +0300

    +++

---
+++ build/CMakeFiles/MyFlip2D.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ship/Coding/MyFlip2D/MyFlip2D/Modules/FluidSolver.cpp" "CMakeFiles/MyFlip2D.dir/Modules/FluidSolver.cpp.o" "gcc" "CMakeFiles/MyFlip2D.dir/Modules/FluidSolver.cpp.o.d"
+  "/home/ship/Coding/MyFlip2D/MyFlip2D/Modules/Utility.cpp" "CMakeFiles/MyFlip2D.dir/Modules/Utility.cpp.o" "gcc" "CMakeFiles/MyFlip2D.dir/Modules/Utility.cpp.o.d"
+  "/home/ship/Coding/MyFlip2D/MyFlip2D/main.cpp" "CMakeFiles/MyFlip2D.dir/main.cpp.o" "gcc" "CMakeFiles/MyFlip2D.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/MyFlip2D.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyFlip2D.dir/Modules/FluidSolver.cpp.o"
+  "CMakeFiles/MyFlip2D.dir/Modules/FluidSolver.cpp.o.d"
+  "CMakeFiles/MyFlip2D.dir/Modules/Utility.cpp.o"
+  "CMakeFiles/MyFlip2D.dir/Modules/Utility.cpp.o.d"
+  "CMakeFiles/MyFlip2D.dir/main.cpp.o"
+  "CMakeFiles/MyFlip2D.dir/main.cpp.o.d"
+  "MyFlip2D"
+  "MyFlip2D.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyFlip2D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
